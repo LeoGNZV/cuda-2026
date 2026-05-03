1b61f8cadbcced9ba2589aa759ca1399c5a37e6e
AlKurpiakov <s23380350@unn.ru>
2026-05-03 14:11:40 +0300

    Changes to be committed: 	modified:   3823B1FI3/4_naive_gemm_cuda/kurpiakov_aleksei/naive_gemm_cuda.cu

--- 3823B1FI3/4_naive_gemm_cuda/kurpiakov_aleksei/naive_gemm_cuda.cu
+++ 3823B1FI3/4_naive_gemm_cuda/kurpiakov_aleksei/naive_gemm_cuda.cu
@@ -62,7 +62,7 @@ std::vector<float> NaiveGemmCUDA(const std::vector<float>& a,
     Kernel<<<grid, tread_net, 0, stream>>>((float*)vec_a, (float*)vec_b, (float*)vec_c, n);
 
 
-    std::vector<float> output(n); 
+    std::vector<float> output(a.size()); 
     cudaMemcpyAsync(output.data(), vec_c, bytes, cudaMemcpyDeviceToHost, stream);
     cudaStreamSynchronize(stream);
     return output;
